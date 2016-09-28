fb5fc69cdc17e8ffcfcd9dd23f6ca215489e4399
victorcobden <vhcobden@gmail.com>
2016-09-28 14:21:40 -0500

    Agregado usuarios conectados

--- controllers/home.js
+++ controllers/home.js
@@ -1,30 +1,52 @@
 var homeController = function(server,io){
 	
+	var users = [];
+
 	io.on('connection',function(socket){
-		console.log('nuevo usuario conectado');
-	});
+		socket.join('chat');
+		socket.on('nuevo usuario',function(data){
+			socket.broadcast.to('chat').emit('devolviendo usuario',data);
+			console.log(data);
+		})
+		
+	});	
+
 
 	server.route('/')
-		.get(function(req,res){
-			if (req.user) {
-				var name = req.user.displayName;
-				var url_foto = 'http://graph.facebook.com/'+req.user.id + '/picture';
-				res.render('home/index',{
-					name:name,
-					foto:url_foto
-				});	
+	.get(function(req,res){
+		if (req.user) {
+			var name = req.user.displayName;
+			var url_foto = 'http://graph.facebook.com/'+req.user.id + '/picture';
+
+			var user = {
+				name : req.user.displayName,
+				foto : req.user.url_foto
 			}
-			else
-			{
-				res.render('home/index');
-			}
+
+			users.push(user);
 			
-		});
+			res.render('home/index',{
+				users:users,
+				name:name,
+				foto:url_foto
+			});
+		}
+		else
+		{
+			res.render('home/index');
+			console.log('usuario no conectado');
+		}
+
+	});
 	server.route('/auth/logout')
-		.get(function(req,res){
-			req.session.destroy();
-			res.redirect('/');
+	.get(function(req,res){
+		io.on('disconnect', function () {
+			console.log('usuario desconectado');
 		});
+		req.session.destroy();
+
+		res.redirect('/');
+	});
 
 };
 
